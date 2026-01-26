e57d4ecbb194a87b1057364f13c22811189c030c
Meek Msaki <meek10x@gmail.com>
2026-01-25 18:42:30 -0600

    create buildernet client

---
+++ packages/buildernet-rpc/src/buildernet-api.ts
@@ -0,0 +1,15 @@
+import { BaseClient } from "@asyncswap/eth-rpc";
+
+export class BuildernetClient extends BaseClient<BuildernetMethodsSpec> {
+  constructor(url: string) {
+    super(url);
+  }
+}
+
+export type FlashbotsRpcMethods<
+  T extends Record<string, { params: unknown[]; result: unknown }>,
+> = {
+    [M in keyof T]: (...params: T[M]["params"]) => Promise<T[M]["result"]>;
+  };
+export interface BuildernetClient
+  extends FlashbotsRpcMethods<BuildernetMethodsSpec> { }
