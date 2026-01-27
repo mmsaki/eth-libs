4b85d55f3baf1196cde1508103f7d03d8ec94489
Meek Msaki <meek10x@gmail.com>
2026-01-26 19:09:40 -0600

    remove package has been renamed

--- packages/types/src/eth/client.ts
+++
@@ -1,8 +0,0 @@
-import type { Uint } from "./base";
-// client.yaml
-export type SyncingStatus = Syncing | false;
-export interface Syncing {
-	startingBlock?: Uint;
-	currentBlock?: Uint;
-	highestBlock?: Uint;
-}
